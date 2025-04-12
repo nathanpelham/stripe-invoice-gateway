856dc5e9f93fc058f93e5bdc389cdc4e3cc43a31
nathanpelham <nathan@gatetechinc.com>
2025-04-11 20:58:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
 console.log('Stripe Gateway Running');
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Stripe Gateway is running 🚀');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
